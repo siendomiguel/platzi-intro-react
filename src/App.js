a5fbdce08e238d670749431878c575c2f685105a
siendomiguel <siendomigueldev@gmail.com>
2022-08-24 09:52:00 -0500

    Creando los componentes de la aplicación

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import { Children } from 'react';
+// import './App.css';
+import { TodoCounter } from './TodoCounter.js'
+import { TodoSearch } from './TodoSearch'
+import { TodoList } from './TodoList'
+import { TodoItem } from './TodoItem'
+import { CreateTodoButton } from './CreateTodoButton'
 
-function App(props) {
+import React from 'react';
+
+
+const todo = [
+  { text: 'Cortar Cebolla', completed: false},
+  { text: 'Tomar curso', completed: false},
+  { text: 'Llorar', completed: false}
+]
+
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React {props.saludo}
-        </a>
-        {props.children}
-      </header>
-    </div>
+    <>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        {todo.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} /> 
+        ))}
+      </TodoList>
+      <CreateTodoButton />
+    </>
   );
 }
 
